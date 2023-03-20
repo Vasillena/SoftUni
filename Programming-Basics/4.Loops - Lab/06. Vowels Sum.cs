58f4fbe89873c6c75e60c82b86586e68d1c01256
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2023-03-21 01:56:08 +0200

    Create 06. Vowels Sum.cs

---
+++ Programming-Basics/4.Loops - Lab/06. Vowels Sum.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Vowels_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            int sum = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char current = text[i];
+
+                switch (current)
+                {
+                    case 'a':
+                        sum += 1;
+                        break;
+                    case 'e':
+                        sum += 2;
+                            break;
+                    case 'i':
+                        sum += 3;
+                        break;
+                    case 'o':
+                        sum += 4;
+                        break;
+                    case 'u':
+                        sum += 5;
+                        break;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}

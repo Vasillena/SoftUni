ffa67380e36be7ba70c066982a17692694c49363
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2023-06-01 00:35:58 +0300

    Add files via upload

---
+++ React/5.Forms/0.ReactJS-Components-Workshop1/client/src/App.js
@@ -0,0 +1,110 @@
+import { useEffect, useState } from 'react';
+
+import * as userService from './services/userService';
+
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Search } from "./components/Search";
+import './App.css';
+import { UserList } from "./components/UserList";
+
+function App() {
+    const [users, setUsers] = useState([]);
+    const [formValues, setFormValues] = useState({
+        firstName: '',
+        lastName: '',
+    });
+    const [formErrors, setFormErros] = useState({
+        firstName: '',
+        lastName: '',
+    });
+
+    useEffect(() => {
+        userService.getAll()
+            .then(setUsers)
+            .catch(err => {
+                console.log('Error' + err);
+            });
+    }, []);
+
+    const onUserCreateSubmit = async (e) => {
+        // stop automatic form submit
+        e.preventDefault();
+
+        // Take form data from DOM tree 
+        const formData = new FormData(e.currentTarget);
+        const data = Object.fromEntries(formData);
+
+        // Send ajax request to server
+        const createdUser = await userService.create(data);
+
+        // If successfull add new user to the state
+        setUsers(state => [...state, createdUser]);
+    };
+
+    const onUserUpdateSubmit = async (e, userId) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const data = Object.fromEntries(formData);
+
+        const updatedUser = await userService.update(userId, data);
+
+        setUsers(state => state.map(x => x._id === userId ? updatedUser : x));
+    }
+
+    const onUserDelete = async (userId) => {
+        // Delete from server
+        await userService.remove(userId);
+
+        // Delete from state
+        setUsers(state => state.filter(x => x._id !== userId));
+    };
+
+    const formChangeHandler = (e) => {
+        setFormValues(state => ({...state, [e.target.name]: e.target.value}))
+    };
+
+    const formValidate = (e) => {
+        console.log('asdasd');
+        const value = e.target.value;
+        const errors = {};
+
+        if (e.target.name === 'firstName' && (value.length < 3 || value.length > 20)) {
+            errors.firstName = 'First name should be between 3 and 20 characters';
+        } 
+
+        if (e.target.name === 'lastName' && (value.length < 3 || value.length > 20)) {
+            errors.lastName = 'Last name should be between 3 and 20 characters';
+        }
+
+        setFormErros(errors);
+    };
+
+    return (
+        <>
+            <Header />
+
+            <main className="main">
+                <section className="card users-container">
+                    <Search />
+
+                    <UserList
+                        users={users}
+                        onUserCreateSubmit={onUserCreateSubmit}
+                        onUserUpdateSubmit={onUserUpdateSubmit}
+                        onUserDelete={onUserDelete}
+                        formValues={formValues}
+                        formChangeHandler={formChangeHandler}
+                        formErrors={formErrors}
+                        formValidate={formValidate}
+                    />
+                </section>
+            </main>
+
+            <Footer />
+        </>
+    );
+}
+
+export default App;

532d803e428e6f2ab366464dc7155d0f40045f3d
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2023-06-18 01:27:21 +0300

    Delete Back-End/10.Exam Preparation directory

--- Back-End/10.Exam Preparation/Crypto-Trade/constants.js
+++
@@ -1,8 +0,0 @@
-exports.SECRET = "Somesecretsecret";
-
-exports.paymentMethodsMap = {
-  "crypto-wallet": "Crypto Wallet",
-  "credit-card": "Credit Card",
-  "debit-card": "Debit Card",
-  paypal: "PayPal",
-};
--- Back-End/10.Exam Preparation/Scaffold/controllers/homeController.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-router.get("/", (req, res) => {
-  res.render("home"); //Автоматично си взема index-a от home папката
-});
-
-module.exports = router;

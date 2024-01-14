266d9e903fd9eeb19e42322dae2da8c750e9769d
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-01-15 00:51:57 +0200

    Create 1-create-tables.sql

---
+++ MySQL/1.Databases Introduction. Data Definition and Datatypes - Exercise/1-create-tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE minions (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    age VARCHAR(50)
+);
+
+CREATE TABLE towns (
+    town_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);

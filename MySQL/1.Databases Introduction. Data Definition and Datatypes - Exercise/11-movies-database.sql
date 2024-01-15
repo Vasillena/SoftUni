0722928bec74475f53d61bcb94d8b438f32101aa
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-01-15 02:08:31 +0200

    Update 11-movies-database.sql

--- MySQL/1.Databases Introduction. Data Definition and Datatypes - Exercise/11-movies-database.sql
+++ MySQL/1.Databases Introduction. Data Definition and Datatypes - Exercise/11-movies-database.sql
@@ -1,35 +1,28 @@
 CREATE TABLE directors (
-id INT AUTO_INCREMENT NOT NULL,
+id INT AUTO_INCREMENT PRIMARY KEY,
 director_name VARCHAR(45) NOT NULL,
-notes TEXT,
-PRIMARY KEY (id));
+notes TEXT);
 
 CREATE TABLE genres (
-id INT AUTO_INCREMENT NOT NULL,
+id INT AUTO_INCREMENT PRIMARY KEY,
 genre_name VARCHAR(45) NOT NULL,
-notes TEXT,
-PRIMARY KEY (id));
+notes TEXT);
 
 CREATE TABLE categories (
-id INT AUTO_INCREMENT NOT NULL,
+id INT AUTO_INCREMENT PRIMARY KEY,
 category_name VARCHAR(45) NOT NULL,
-notes TEXT,
-PRIMARY KEY (id));
+notes TEXT);
 
 CREATE TABLE movies (
-id INT AUTO_INCREMENT NOT NULL,
+id INT AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(45) NOT NULL,
-director_id INT NOT NULL,
-copyright_year YEAR,
-length INT,
-genre_id INT NOT NULL,
-category_id INT NOT NULL,
+director_id INT,
+copyright_year DATE,
+length INT(4),
+genre_id INT,
+category_id INT,
 rating INT,
-notes TEXT,
-PRIMARY KEY (id),
-FOREIGN KEY (director_id) REFERENCES directors(id),
-FOREIGN KEY (genre_id) REFERENCES genres(id),
-FOREIGN KEY (category_id) REFERENCES categories(id));
+notes TEXT);
 
 INSERT INTO directors (director_name, notes) VALUES
 ('Test1', NULL),
@@ -52,9 +45,9 @@ INSERT INTO categories (category_name, notes) VALUES
 ('Test444', NULL),
 ('Test555', NULL);
 
-INSERT INTO movies (title, director_id, copyright_year, length, genre_id, category_id, rating, notes) VALUES
-('Test1111', 1, 2000, 120, 1, 1, 5, NULL),
-('Test2222', 2, 2000, 120, 2, 2, 5, NULL),
-('Test3333', 3, 2000, 120, 3, 3, 5, NULL),
-('Test4444', 4, 2000, 120, 4, 4, 5, NULL),
-('Test5555', 5, 2000, 120, 5, 5, 5, NULL);
+INSERT INTO movies (title, copyright_year, rating, notes) VALUES
+('Test1111', '2000-01-10', '5', NULL),
+('Test2222', '2000-02-11', '5', NULL),
+('Test3333', '2000-03-12', '5', NULL),
+('Test4444', '2000-04-13', '5', NULL),
+('Test5555', '2000-05-14', '5', NULL);

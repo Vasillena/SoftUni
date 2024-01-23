babc8a31fac67f4e834fcbf6de042418d31cd353
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-01-23 21:24:33 +0200

    Create 4-delete-mountains.sql

---
+++ MySQL/5.Table Relations - Lab/4-delete-mountains.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `mountains`(
+    `id`   INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50)
+);
+
+CREATE TABLE `peaks`
+(
+    `id`          INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name`        VARCHAR(50),
+    `mountain_id` INT,
+    CONSTRAINT `fk_peaks_mountains`
+        FOREIGN KEY (`mountain_id`)
+            REFERENCES `mountains` (`id`)
+            on DELETE CASCADE
+);

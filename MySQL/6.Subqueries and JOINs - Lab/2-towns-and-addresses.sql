86c3c16da4d1ba866b4e72676c33a5dfff2309a8
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-01-30 00:57:15 +0200

    Update 2-towns-and-addresses.sql

--- MySQL/6.Subqueries and JOINs - Lab/2-towns-and-addresses.sql
+++ MySQL/6.Subqueries and JOINs - Lab/2-towns-and-addresses.sql
@@ -1,15 +1,10 @@
-SELECT *
-FROM `addresses`;
-
-SELECT *
-FROM `towns`;
-
-SELECT 
-    `t`.`town_id`, `t`.`name` AS 'town_name', `a`.`address_text`
-FROM
-    `towns` AS `t`
-        JOIN
-    `addresses` AS `a` ON `t`.`town_id` = `a`.`town_id`
-WHERE
-    `t`.`town_id` IN (9 , 15, 32)
-ORDER BY `t`.`town_id` , `a`.`address_id`;
+SELECT
+    `a`.`town_id`,
+    `t`.`name`,
+    `a`.`address_text`
+FROM `addresses` AS `a`
+JOIN 
+      `towns` AS `t` ON `a`.`town_id` = `t`.`town_id`
+    WHERE
+    `t`.`name` IN ('San Francisco' , 'Sofia', 'Carnation')
+ORDER BY `a`.`town_id` , `a`.`address_id`;

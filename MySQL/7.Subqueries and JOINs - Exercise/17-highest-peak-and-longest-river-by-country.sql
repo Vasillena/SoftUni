bafa6325a61cff6daad6e37b471b4e64e7bf9051
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-01-30 15:19:18 +0200

    Create 17-highest-peak-and-longest-river-by-country.sql

---
+++ MySQL/7.Subqueries and JOINs - Exercise/17-highest-peak-and-longest-river-by-country.sql
@@ -0,0 +1,19 @@
+SELECT 
+    `c`.`country_name`,
+    MAX(`p`.`elevation`) AS 'highest_peak_elevation',
+    MAX(`r`.`length`) AS 'longest_river_length'
+FROM
+    `countries` AS `c`
+        LEFT JOIN
+    `mountains_countries` AS `mc` ON `c`.`country_code` = `mc`.`country_code`
+        LEFT JOIN
+    `mountains` AS `m` ON `mc`.`mountain_id` = `m`.`id`
+        LEFT JOIN
+    `peaks` AS `p` ON `m`.`id` = `p`.`mountain_id`
+        LEFT JOIN
+    `countries_rivers` AS `cr` ON `c`.`country_code` = `cr`.`country_code`
+        LEFT JOIN
+    `rivers` AS `r` ON `r`.`id` = `cr`.`river_id`
+GROUP BY `c`.`country_name`
+ORDER BY `highest_peak_elevation` DESC , `longest_river_length` DESC , `c`.`country_name` ASC
+LIMIT 5;

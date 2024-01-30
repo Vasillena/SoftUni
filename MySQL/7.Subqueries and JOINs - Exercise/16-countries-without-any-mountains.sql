97434695241b04928e95f068417bfda45e420a63
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-01-30 15:18:01 +0200

    Create 16-countries-without-any-mountains.sql

---
+++ MySQL/7.Subqueries and JOINs - Exercise/16-countries-without-any-mountains.sql
@@ -0,0 +1,8 @@
+SELECT 
+    COUNT(*) AS 'country_count'
+FROM
+    `countries` AS `c`
+        LEFT JOIN
+    `mountains_countries` AS `mc` ON `c`.`country_code` = `mc`.`country_code`
+WHERE
+    `mc`.`mountain_id` IS NULL;

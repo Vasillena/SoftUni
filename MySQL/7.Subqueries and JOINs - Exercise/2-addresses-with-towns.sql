6bb01ac9dfa09a25cac0b8e25d4fa13f78b76f56
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-01-30 14:31:22 +0200

    Create 2-addresses-with-towns.sql

---
+++ MySQL/7.Subqueries and JOINs - Exercise/2-addresses-with-towns.sql
@@ -0,0 +1,13 @@
+SELECT 
+    `e`.`first_name`,
+    `e`.`last_name`,
+    `t`.`name` AS 'town',
+    `a`.`address_text`
+FROM
+    `employees` AS `e`
+        JOIN
+    `addresses` AS `a` ON `e`.`address_id` = `a`.`address_id`
+        JOIN
+    `towns` AS `t` ON `a`.`town_id` = `t`.`town_id`
+ORDER BY `e`.`first_name` , `e`.`last_name`
+LIMIT 5;

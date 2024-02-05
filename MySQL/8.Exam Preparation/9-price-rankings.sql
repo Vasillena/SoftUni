7a45ab33c873398e971b9d92ac67ac75d6425f6f
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-02-05 19:58:42 +0200

    Create 9-price-rankings.sql

---
+++ MySQL/8.Exam Preparation/9-price-rankings.sql
@@ -0,0 +1,16 @@
+SELECT
+	`u`.`name` AS 'university_name',
+    `c`. `name` AS 'city_name',
+    `u`.`address`,
+    CASE
+		WHEN `u`.`tuition_fee` < 800
+			THEN 'cheap'
+		WHEN `u`.`tuition_fee` >= 800 AND `u`.`tuition_fee` < 1200
+			THEN 'normal'
+		WHEN `u`.`tuition_fee` >= 1200 AND `u`.`tuition_fee` < 2500 
+			THEN 'high'
+		ELSE 'expensive'
+	END AS 'price_rank' ,  `u`.`tuition_fee`
+FROM `universities` AS `u`
+JOIN `cities` AS `c` ON `u`.`city_id` = `c`.`id`
+ORDER BY `u`.`tuition_fee` ASC;

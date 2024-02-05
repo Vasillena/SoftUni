e0f9a3c2f73b90365c21b23742cc2d715b4172ce
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-02-05 16:16:12 +0200

    Create 4-employees-from-town.sql

---
+++ MySQL/7.Database Programmability and Transactions - Exercises/4-employees-from-town.sql
@@ -0,0 +1,16 @@
+DELIMITER $ 
+  CREATE PROCEDURE `usp_get_employees_from_town` (`town_name` VARCHAR(50)) 
+  BEGIN
+SELECT 
+  `first_name`,
+  `last_name`
+FROM `employees` AS `e`
+    JOIN `addresses` AS `a` ON `e`.`address_id` = `a`.`address_id`
+    JOIN `towns` AS `t` ON `a`.`town_id` = `t`.`town_id`
+WHERE `t`.`name` = `town_name`
+ORDER BY 
+  `e`.`first_name`,
+  `e`.`last_name`,
+  `e`.`employee_id`;
+END $ 
+  DELIMITER;

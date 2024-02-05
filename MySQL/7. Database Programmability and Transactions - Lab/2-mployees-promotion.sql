1e9901240a9aade4b3d0d99e766d3a598e4c01e4
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-02-05 15:41:46 +0200

    Create 2-mployees-promotion.sql

---
+++ MySQL/7. Database Programmability and Transactions - Lab/2-mployees-promotion.sql
@@ -0,0 +1,11 @@
+DELIMITER $
+CREATE PROCEDURE `usp_raise_salaries`(`department_name` VARCHAR(50))
+BEGIN
+	UPDATE `employees` AS `e`
+    JOIN `departments` AS `d` ON `e`. `department_id`= `d`. `department_id`
+    SET `e`. `salary` = `e`. `salary` * 1.05
+    WHERE `d`. `name` = `department_name`;
+END$
+
+DELIMITER ;
+;

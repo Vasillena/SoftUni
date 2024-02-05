224967e6605b3d2e95b08f1d803ffa9fd0003c38
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-02-05 16:19:18 +0200

    Create 7-define-function.sql

---
+++ MySQL/7.Database Programmability and Transactions - Exercises/7-define-function.sql
@@ -0,0 +1,8 @@
+DELIMITER $
+CREATE FUNCTION `ufn_is_word_comprised`(`set_of_letters` VARCHAR(50), `word` VARCHAR(50))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+     RETURN `word` REGEXP (CONCAT('^[', `set_of_letters`, ']+$'));
+END $
+DELIMITER ;
